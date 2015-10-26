160b323f353ddecc7f313c4732aa911a5294845f
Armour Lu <armour.lu@gmail.com>
2015-10-26 15:55:09 +0800

    Update the process of querying data by datetime

--- dht_json.php
+++ dht_json.php
@@ -4,6 +4,7 @@ include "/var/www_private/mysql_conn.php";
 $start = microtime(true);
 
 $max_query_count = 60 * 60 * 24; // 1 day
+$query_interval = 1;
 
 //Get count
 $query_count = $_GET['c'];
@@ -14,7 +15,7 @@ if($query_count>$max_query_count) $query_count = $max_query_count;
 $query_group = $_GET['g'];
 if($query_group=='' || !is_numeric($query_group))
 {
-    $sqlstr = "SELECT ID FROM dht_group order by ID desc";
+    $sqlstr = "SELECT ID FROM dht_group order by ID desc limit 0,1";
     $result = mysql_query($sqlstr);
     $row = mysql_fetch_row($result);
     $query_group = $row[0];
@@ -36,43 +37,33 @@ else
 	if($query_sensor=='' || !is_numeric($query_sensor)) $query_sensor=0;
 }
 
-//Get from id
-$query_from = $_GET['f'];
-
-//Get end id
-$query_end = $_GET['e'];
-
-//Get from date
-$query_from_datetime = $_GET['fd'];
-if (date('Y-m-d H:i:s', strtotime($query_from_datetime)) != $query_from_datetime) $query_from_datetime="";
-else $query_count = $max_query_count;
-
 //Get end date
 $query_end_datetime = $_GET['ed'];
-if (date('Y-m-d H:i:s', strtotime($query_end_datetime)) != $query_end_datetime) $query_end_datetime="";
-else $query_count = $max_query_count;
 
-$query_fields = "ID, replace(replace(replace(replace(Reading,'C,H:',','),'%,T:',','),'T:',''),'%,',''), UNIX_TIMESTAMP(DateTime)";
+if (date('Y-m-d H:i:s', strtotime($query_end_datetime))!= $query_end_datetime)
+{
+    $query_end_datetime="";
+}
+else
+{
+    $query_from_datetime = date("Y-m-d H:i:s",strtotime($query_end_datetime) - $query_count + 1);
+    
+    $sqlstr = "select ID from dht where datetime <= '$query_end_datetime' and GroupID=$query_group order by id desc limit 0,1";
+    $result = mysql_query($sqlstr);
+    $row = mysql_fetch_row($result);
+    $query_end = $row[0];
+    
+    $sqlstr = "select ID from dht where datetime >= '$query_from_datetime' and GroupID=$query_group order by id limit 0,1";
+    $result = mysql_query($sqlstr);
+    $row = mysql_fetch_row($result);
+    $query_from = $row[0];
+}
+
+$query_fields = "ID, Reading, UNIX_TIMESTAMP(DateTime)";
 
 // Query by datetime
-if($query_from_datetime != "" && $query_end_datetime !=""){
-	$sqlstr = "select $query_fields from dht where datetime >= '$query_from_datetime' and datetime <= '$query_end_datetime' and GroupID=$query_group order by id desc";
-}
-else if($query_from_datetime != ""){
-	$sqlstr = "select $query_fields from dht where datetime >= '$query_from_datetime' and GroupID=$query_group order by id";
-}
-else if($query_end_datetime != ""){
-	$sqlstr = "select $query_fields from dht where datetime <= '$query_end_datetime' and GroupID=$query_group order by id desc limit 0,$query_count";
-}
-//Query by ID
-else if(is_numeric($query_from) && is_numeric($query_end)){
-	$sqlstr = "select $query_fields from dht where id >= $query_from and id <= $query_end and GroupID=$query_group order by id desc";
-}
-else if(is_numeric($query_from)){
-	$sqlstr = "select $query_fields from dht where id >= $query_from and GroupID=$query_group order by id";
-}
-else if(is_numeric($query_end)){
-	$sqlstr = "select $query_fields from dht where id <= $query_end and GroupID=$query_group order by id desc limit 0,$query_count";
+if($query_end_datetime != ""){
+	$sqlstr = "select $query_fields from dht where id <= $query_end and id >= $query_from and GroupID=$query_group order by id desc";
 }
 else{
 	$sqlstr = "select $query_fields from dht where GroupID=$query_group order by id desc limit 0,$query_count";
@@ -86,23 +77,26 @@ $DhtArray = array();
 // Loop for fetching date from mysql
 $LastID = 0;
 $LastDate = 0;
+$DhtArrayCount = 0;
+$rowindex = 0;
+
 $PreDate = 0;
-$CurrDate = 0;
 $MissCount = 0;
 $rowindex = 0;
-$DhtArrayCount = 0;
 
-while ($row = mysql_fetch_array($result)) {
+$start = microtime(true);
+while ($row = mysql_fetch_row($result)) {
+	if($DhtArrayCount>=$query_count) break;
+    $rowindex++;
+
     $readings = explode(',',$row[1]);
-    $readings_count = count($readings)/2;
-    
+
     if($DhtArrayCount == 0)
     {
+        $readings_count = count($readings)/2;
         $LastID = (int)$row[0];
         $LastDate = $row[2];
     }
-	if($DhtArrayCount>=$query_count) break;
-    $rowindex++;
 
     if($DhtArrayCount>0 && $PreDate > $row[2]+1 )
     {
@@ -110,37 +104,30 @@ while ($row = mysql_fetch_array($result)) {
         $MissCount += $MissingSecond;
     }
     else $MissingSecond=0;
-    
     $PreDate = $row[2];
-		
+
 	if(is_numeric($query_sensor)){
         $tmpArray = array();
-		// Get temperature
         if($readings_count < $query_sensor+1)
         {
-            $row_array[0] = 0;
-            $row_array[1] = 0;
+            $tmpArray[0] = [0,0];
         }
         else
         {
-            $row_array[0] = (float)$readings[$query_sensor*2];
-            $row_array[1] = (float)$readings[$query_sensor*2+1];
+            $tmpArray[0] = [(float)$readings[$query_sensor*2],(float)$readings[$query_sensor*2+1]];
         }
-        $tmpArray[0] = [$row_array[0],$row_array[1]];
 	}
 	else
 	{
         $tmpArray = array();
 		for($i=0 ; $i < $readings_count ; $i++){
-			$row_array[0] = (float)$readings[$i*2];
-			$row_array[1] = (float)$readings[$i*2+1];
-            $tmpArray[$i] = [$row_array[0],$row_array[1]];
+            $tmpArray[$i] = [(float)$readings[$i*2],(float)$readings[$i*2+1]];
 		}
 	}
     
     $DhtArray[$DhtArrayCount] = $tmpArray;
-    $DhtArrayCount ++;
-    
+    $DhtArrayCount++;
+
     for($i=0;($i<$MissingSecond)&&($DhtArrayCount < $query_count);$i++)
     {
         $DhtArray[$DhtArrayCount] = $tmpArray;
@@ -148,7 +135,7 @@ while ($row = mysql_fetch_array($result)) {
     }
 }
 
-$returnjson['TotalTime'] = number_format((microtime(true) - $start), 2) . "s";
+$returnjson['ParseTime'] = number_format((microtime(true) - $start), 2) . "s";
 $returnjson['Status'] = "OK";
 $returnjson['LastID'] = $LastID;
 $returnjson['LastDate'] = date("Y-m-d H:i:s",$LastDate);
@@ -156,8 +143,7 @@ $returnjson['Count'] = count($DhtArray);
 $returnjson['SensorCount'] = count($DhtArray[0]);
 $returnjson['Group'] = $query_group;
 $returnjson['Sensor'] = $query_sensor;
-//$returnjson['MissCount'] = $MissCount;
-//$returnjson['rowindex'] = $rowindex;
+$returnjson['MissCount'] = $MissCount;
 $returnjson['Data'] = $DhtArray;
 echo json_encode($returnjson);
 
